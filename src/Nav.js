a94741182e430c13b37ad68852616bf759edd009
Prawinpravs <79414071+Prawinpravs@users.noreply.github.com>
2021-05-17 21:57:22 +0530

    final commit

--- src/Nav.js
+++ src/Nav.js
@@ -18,10 +18,10 @@ function Nav() {
     return (
         <div className ={`navbar ${show && 'navbar_black'}`}>
             <img className="navbar_logo"        
-                src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"alt="Netflix Logo">
+                src="netflix.png"alt="Netflix Logo">
             </img>  
             
-            <img className="navbar_signUp" src ="https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg" alt="PrawinPravs">
+            <img className="navbar_signUp" src ="ampu.png" alt="PrawinPravs">
                 {/* src="https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg" alt="PrawinPravs"> */}
                 </img>
         </div>
